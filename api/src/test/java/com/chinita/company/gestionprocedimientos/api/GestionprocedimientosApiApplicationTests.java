6f3d30a5c083210327bba8487be15fc7794daeb1
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2020-04-12 10:49:32 -0500

    Configuración inicial

---
+++ api/src/test/java/com/chinita/company/gestionprocedimientos/api/GestionprocedimientosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinita.company.gestionprocedimientos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionprocedimientosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
