140ebf1de8173a50a8812d64273b442bc6c8e5da
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2020-04-13 19:12:24 -0500

    faltó anotación repository

--- api/src/main/java/com/chinita/company/gestionprocedimientos/api/Repository/PersonaRepository.java
+++ api/src/main/java/com/chinita/company/gestionprocedimientos/api/Repository/PersonaRepository.java
@@ -2,6 +2,8 @@ package com.chinita.company.gestionprocedimientos.api.Repository;
 
 import com.chinita.company.gestionprocedimientos.api.Modelo.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonaRepository extends JpaRepository<Persona, Integer> {
 }
