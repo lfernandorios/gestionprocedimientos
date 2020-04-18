e308e51fd23ec9d302e83b793468a149d31d75c8
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2020-04-18 10:31:55 -0500

    mapeo del dto en personaServicio y personaControler, adición de bean en el app y cambio en la url del controler

--- api/src/main/java/com/chinita/company/gestionprocedimientos/api/repository/PersonaRepository.java
+++ api/src/main/java/com/chinita/company/gestionprocedimientos/api/repository/PersonaRepository.java
@@ -1,9 +1,9 @@
 package com.chinita.company.gestionprocedimientos.api.repository;
 
 import com.chinita.company.gestionprocedimientos.api.modelo.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonaRepository extends JpaRepository<Persona, Integer> {
+public interface PersonaRepository extends CrudRepository<Persona, Long> {
 }
